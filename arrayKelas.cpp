faba0226ce36f5167f4fc017c757b2d94af1b053
akufarish <risfarishk@gmail.com>
2025-03-27 13:51:19 +0800

    Pembuatan procedure tampilkan data array dan pengujian

--- arrayKelas.cpp
+++ arrayKelas.cpp
@@ -4,13 +4,33 @@ using namespace std;
 string kelas[30];
 int pos = 0;
 void inputData(string nama);
+void outputData();
 
 int main() {
     inputData("Farish Asqalani");
+    outputData();
+    inputData("Mie Ayam sedap");
+    outputData();
+    inputData("Minum air putih");
+    outputData();
     return 0;
 }
 
 void inputData(string nama) {
     kelas[pos] = nama;
     pos++;
+}
+
+void outputData() {
+    for (int i = 0; i < pos; i++)
+    {
+        cout << kelas[i];
+
+        if (i < pos - 1) {
+            cout << ",";
+        } else {
+            cout << "." << endl;
+        }
+    }
+    
 }
